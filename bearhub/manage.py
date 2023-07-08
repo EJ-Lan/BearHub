9046db147870b3d6bdf8bcb6b41228bd6de4bbd5
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-08 01:31:47 -0600

    Fix Django Project Configuration in Root Folder

--- bearhub/manage.py
+++ bearhub/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temp_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bearhub.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
