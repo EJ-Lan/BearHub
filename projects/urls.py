6b43d1688f82e1eb061342311aa07ac1cf45f4d4
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-09 15:19:27 -0600

    Test out some django features with sample data

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-        path('projects/', views.projects, name="projects"),
+    path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 def projects(requests):
-    return render(requests, 'projects/projects.html')
+    page = "projects"
+    number = 10
+    context = {'page':page, 'number':number, 'projects': projectsList}
+    return render(requests, 'projects/projects.html', context)
 
 def project(requests, pk):
-    return render(requests, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(requests, 'projects/single-project.html', {'project':projectObj})
