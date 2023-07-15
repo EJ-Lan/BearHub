e7baa37aea96c00c19c9de181f4006c777666fe0
Ervin Lanada <ervinjon_lanada@outlook.com>
2023-07-15 04:08:50 -0600

    Use UI Kit and Work on Templates and Styling

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
