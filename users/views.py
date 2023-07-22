fabde18922556801ca2767f8a1f0df6490de6d44
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-22 03:43:04 -0600

    Finish Engineers Page and Single Engineer Page

--- users/views.py
+++ users/views.py
@@ -9,5 +9,9 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile': profile}
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
